6178555cbc80cc6611f09f87fe685731df722bc7
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-09 15:06:01 +0200

    [ADD]: AppHero component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+name: "AppHero"
+}
+</script>
+
+
+<template>
+  <img src="/src/assets/img/04.webp" alt="">
+</template>
+
+
+<style>
+img {
+    width: 100%;
+}
+</style>
